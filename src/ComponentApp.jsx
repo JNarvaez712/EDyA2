c0e8251933de9eb5633bc2e3a749fce9ed81e61d
Jonathan Steven Narvaez Navia <jonathan.narvaez@uao.edu.co>
2024-10-09 16:58:37 -0500

    Challenge 3

---
+++ src/ComponentApp.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+
+export const GitExpertApp = () => {
+    const [categories, setCategories] = useState(['primer categoria'])
+    const [category, setCategory] = useState('')
+
+    const onAddCategory = () => {
+        setCategories(list=>[...list, category])
+        setCategory('')
+    }
+
+    const onSetCategory = (evt) => {
+        setCategory(evt.target.value)
+    }
+
+    return (
+        <>
+            <h1>GifExpert</h1>
+            <input 
+                type="text" value={category}
+                onChange={(event)=>onSetCategory(event)}/>
+            <button onClick={()=>onAddCategory()}>Agregar Categoria</button>
+            <ol>
+                {
+                    categories.map(
+                        (category, key) =>
+                            {
+                                return <li key={key}>{category}</li>
+                            }
+                    )
+                }
+            </ol>
+        </>
+    )
+}
+export default GitExpertApp
