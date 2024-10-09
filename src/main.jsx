51981eccfe701d52464391653c1860a9e500bbab
Jonathan Steven Narvaez Navia <jonathan.narvaez@uao.edu.co>
2024-10-09 17:51:14 -0500

    Challenge 6

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { useFetch } from './hooks/useFetch.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <useFetch />
   </StrictMode>,
 )
